eab0eab93bf2d869b22d5ad45f28e3a8bdd8125a
Navneet <navneetgurjar2252@gmail.com>
2026-05-19 08:28:54 +0530

    Added home route

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const connectToDB = require("./src/config/databse")
 
 connectToDB()
 
+app.get("/", (req,res)=>{
+    res.status(200).json({
+        message : "URL Shortener Backend Running 🚀"
+    })
+})
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
